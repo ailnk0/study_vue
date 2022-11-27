a366fa7ed9852037d6e7dbaaea67cbd1f8d0da6d
Benedict <ailnk@naver.com>
2022-11-28 01:25:01 +0900

    [EX] Simple export/import
    - data export/import 연습

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -15,33 +15,27 @@
 
   <div v-for="p in products" :key="p" :style="itemStyle">
     <img class="room-img" :src="p.image">
-    <h4 @click="toggleMsg">{{ p.name }}</h4>
+    <h4 @click="toggleMsg">{{ p.title }}</h4>
     <p>{{ p.price }} 만원</p>
-    <button @click="incRpt(p)">허위매물신고</button>
-    <span> 신고수 : {{ p.rpt }} </span>
   </div>
 
 </template>
 
 <script>
+
+import data from './rooms.js';
+
 export default {
   name: 'App',
   data() {
     return {
       isMsgShow: false,
       menuItems: ['Home', 'Products', 'About'],
-      products: [
-        { name: '역삼동원룸', price: 50, rpt: 0, image: require('./assets/room0.jpg') },
-        { name: '천호동원룸', price: 60, rpt: 0, image: require('./assets/room1.jpg') },
-        { name: '마포구원룸', price: 70, rpt: 0, image: require('./assets/room2.jpg') }
-      ],
+      products: data,
       itemStyle: 'color : darkslateblue'
     }
   },
   methods: {
-    incRpt(p) {
-      p.rpt++;
-    },
     toggleMsg() {
       this.isMsgShow = !this.isMsgShow;
     }
