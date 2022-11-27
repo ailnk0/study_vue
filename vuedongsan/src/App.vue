660dafe0ca0b4923683c88177b4fa290da6b17db
Benedict <ailnk@naver.com>
2022-11-28 00:05:28 +0900

    [EX] Simple Data Binding
    - Bind Data
    - Bind Style

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :style="selectStyle">
+    <h4>{{ products[0].name }}</h4>
+    <p>{{ products[0].price }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1].name }}</h4>
+    <p>{{ products[1].price }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2].name }}</h4>
+    <p>{{ products[2].price }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data() {
+    return {
+      products: [
+        { name: '역삼동원룬', price: 50 },
+        { name: '천호동원룸', price: 60 },
+        { name: '마포구원룸', price: 70 }
+      ],
+      selectStyle: 'color : blue'
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
