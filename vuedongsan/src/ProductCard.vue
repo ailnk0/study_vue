6096b4d798f15530a45b86728ceb76642e58d8c0
Benedict <ailnk@naver.com>
2022-11-28 23:11:54 +0900

    [EX] Simple Props 3
    - v-for를 사용하는 component에 prop로 데이터 읽기 연습

---
+++ vuedongsan/src/ProductCard.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+        <img class="room-img" :src="p.image">
+        <h4 @click="toggleDetail(i)">{{ p.title }}</h4>
+        <p>{{ p.price }}원</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name : 'ProductCard',
+    props : {
+        p : Object,
+        i : Number,
+        toggleDetail : Function
+    }
+}
+</script>
+
+<style>
+</style>
