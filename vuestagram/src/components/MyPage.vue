f1fdf4ebb8666836e9eb5c9674557d1729d0ea22
Benedict <ailnk@naver.com>
2022-12-05 01:30:08 +0900

    [EX] Composite API 2
    - 팔로워 검색 기능 추가
    - Composite API 연습

--- vuestagram/src/components/MyPage.vue
+++ vuestagram/src/components/MyPage.vue
@@ -2,8 +2,8 @@
   <div>
     <div style="padding : 10px">
       <h4>팔로워</h4>
-      <input placeholder="🔎" />
-      <div class="post-header d-flex" v-for="f in follower" :key="f">
+      <input @input="search($event.target.value)" placeholder="🔎" />
+      <div class="post-header d-flex" v-for="f in followerView" :key="f">
         <div class="post-profile" :style="{ backgroundImage: `url(${f.image})` }"></div>
         <span class="post-profile-name">{{ f.name }}</span>
       </div>
@@ -18,15 +18,25 @@ import { onMounted, ref } from 'vue';
 export default {
   setup() {
     let follower = ref([]);
+    let followerView = ref([]);
+
+    function search(key) {
+      followerView.value = follower.value.filter((item) => {
+        return item.name.includes(key);
+      });
+    }
 
     onMounted(() => {
       axios.get('/follower.json').then((response) => {
         follower.value = response.data;
+        followerView.value = response.data;
       });
     });
 
     return {
-      follower
+      follower,
+      followerView,
+      search,
     }
   },
   data() {
