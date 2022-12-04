930a93a9e14a925ba970cdb2c1b9f812f6ef4aab
Benedict <ailnk@naver.com>
2022-12-04 22:44:37 +0900

    [EX] Simple Vuex Actions
    - ajax 요청과 같이 오래걸릴 수 있는 기능은 mutation이 아니라 action을 사용
    - get more post action 연습

--- vuestagram/src/store.js
+++ vuestagram/src/store.js
@@ -1,15 +1,32 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
   state() {
     return {
-      count: 0
+      count: 0,
+      more_post: {},
+      more_count: 0,
     }
   },
   mutations: {
     increment(state) {
       state.count++
+    },
+    setMore(state, data) {
+      state.more_post = data;
+      state.more_count++;
+    }
+  },
+  actions: {
+    getMore(context) {
+      axios.get(`https://codingapple1.github.io/vue/more${context.state.more_count}.json`)
+        .then((response) => {
+          context.commit('setMore', response.data);
+        })
+        .catch((/*error*/) => {
+        });
     }
   }
 })
