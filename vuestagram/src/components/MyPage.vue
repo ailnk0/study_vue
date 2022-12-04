3ab8b505d62bede6f776e06b2f814c556bd2e8b9
Benedict <ailnk@naver.com>
2022-12-05 01:03:34 +0900

    [EX] Composite API
    - Composite API 연습
    - 내 정보에서 팔로워 보기 기능 추가

---
+++ vuestagram/src/components/MyPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div style="padding : 10px">
+      <h4>팔로워</h4>
+      <input placeholder="🔎" />
+      <div class="post-header d-flex" v-for="f in follower" :key="f">
+        <div class="post-profile" :style="{ backgroundImage: `url(${f.image})` }"></div>
+        <span class="post-profile-name">{{ f.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+export default {
+  setup() {
+    let follower = ref([]);
+
+    onMounted(() => {
+      axios.get('/follower.json').then((response) => {
+        follower.value = response.data;
+      });
+    });
+
+    return {
+      follower
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
