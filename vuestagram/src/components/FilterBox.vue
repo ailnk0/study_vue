03a2d65413098c89ca085baa21b082e58bc9426f
Benedict <ailnk@naver.com>
2022-12-04 21:32:07 +0900

    [EX] Simple CSSGram
    - CSSGram을 이용한 필터 적용
    - Class Data Binding 연습

---
+++ vuestagram/src/components/FilterBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="`filter-box ${filter}`" :style="{ backgroundImage: `url(${upload_img_url})` }"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  props: {
+    filter: String,
+    upload_img_url: String,
+  }
+}
+</script>
+
+<style>
+.filter-box {
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+}
+</style>
